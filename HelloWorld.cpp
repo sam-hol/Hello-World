beaa4d4a9423a40edcc61d715f2963718b1c55e3
sylandris <sarious6@gmail.com>
2021-03-21 15:16:51 -0400

    Add project files.

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+Sam Hollinger
+Hello World
+3/21/21
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!";
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
+
